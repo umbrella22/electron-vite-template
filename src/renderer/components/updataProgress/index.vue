8630423e317b811aecb3d9274cd6c3c02a98e9b6
umbrella22 <1035765115@qq.com>
2021-03-24 17:47:02 +0800

    更新弹窗占坑

---
+++ src/renderer/components/updataProgress/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      
+  </div>
+</template>
+
+<script>
+import { defineComponent, } from "vue";
+export default defineComponent({
+  components: {},
+  setup: () => {
+    return {};
+  },
+});
+</script>
+
+<style scoped lang="scss">
+</style>
