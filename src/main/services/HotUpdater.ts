cd85c24b1a8bf9de210ead82952c684254c0e344
umbrella22 <1035765115@qq.com>
2021-04-12 15:33:43 +0800

    修正错误

--- src/main/services/HotUpdater.ts
+++ src/main/services/HotUpdater.ts
@@ -60,32 +60,32 @@ const updateInfo = {
  */
 export const updater = async (windows?: BrowserWindow) => {
     try {
-            const res = await request({ url: `${hotPublishConfig.url}/${hotPublishConfig.configName}?time=${new Date().getTime()}`, })
-            if (gt(res.data.version, version)) {
-                await emptyDir(updatePath)
-                const filePath = join(updatePath, res.data.name)
-                updateInfo.status = 'downloading'
-                if (windows) windows.webContents.send('hot-update-status', updateInfo);
-                await download(hotPublishConfig.url, filePath)
-                const buffer = await readFile(filePath)
-                const sha256 = hash(buffer)
-                if (sha256 !== res.data.hash) throw new Error('sha256 error')
-                const appPathTemp = join(updatePath, 'temp')
-                await extract(filePath, { dir: appPathTemp })
-                updateInfo.status = 'moving'
-                if (windows) windows.webContents.send('hot-update-status', updateInfo);
-                await emptyDir(appPath)
-                await copy(appPathTemp, appPath)
-                updateInfo.status = 'finished'
-                if (windows) windows.webContents.send('hot-update-status', updateInfo);
-                resolve('success')
-            }
+        const res = await request({ url: `${hotPublishConfig.url}/${hotPublishConfig.configName}?time=${new Date().getTime()}`, })
+        if (gt(res.data.version, version)) {
+            await emptyDir(updatePath)
+            const filePath = join(updatePath, res.data.name)
+            updateInfo.status = 'downloading'
+            if (windows) windows.webContents.send('hot-update-status', updateInfo);
+            await download(hotPublishConfig.url, filePath)
+            const buffer = await readFile(filePath)
+            const sha256 = hash(buffer)
+            if (sha256 !== res.data.hash) throw new Error('sha256 error')
+            const appPathTemp = join(updatePath, 'temp')
+            await extract(filePath, { dir: appPathTemp })
+            updateInfo.status = 'moving'
+            if (windows) windows.webContents.send('hot-update-status', updateInfo);
+            await emptyDir(appPath)
+            await copy(appPathTemp, appPath)
+            updateInfo.status = 'finished'
+            if (windows) windows.webContents.send('hot-update-status', updateInfo);
+            resolve('success')
+        }
 
 
     } catch (error) {
         updateInfo.status = 'failed'
         updateInfo.message = error
-        windows.webContents.send('hot-update-status', updateInfo)
+        if (windows) windows.webContents.send('hot-update-status', updateInfo)
     }
 }
 
