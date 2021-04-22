c130be07164f97513ce3952a691d409ba3550843
umbrella22 <1035765115@qq.com>
2021-04-22 23:42:40 +0800

    修改404页面中没有正确使用ts导致volar报错，添加图片类型

--- src/renderer/views/404.vue
+++ src/renderer/views/404.vue
@@ -18,7 +18,7 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import img_404 from "@renderer/assets/404_images/404.png";
 import img_404_cloud from "@renderer/assets/404_images/404_cloud.png";
 import { defineComponent } from "vue";
