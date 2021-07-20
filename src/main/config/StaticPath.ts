33941c6c74bc6057e67c5154ccd9f90d5d87fb12
umbrella22 <1035765115@qq.com>
2021-07-20 09:58:39 +0800

    修正路径

--- src/main/config/StaticPath.ts
+++ src/main/config/StaticPath.ts
@@ -2,17 +2,14 @@
 import { join } from 'path'
 import { DllFolder, HotUpdateFolder } from '@config/index'
 
-var __static: string
-var __lib: string
-var __updateFolder: string
 if (process.env.NODE_ENV !== 'development') {
   process.env.__static = join(__dirname, '..', 'renderer').replace(/\\/g, '\\\\')
   process.env.__lib = join(__dirname, '..', '..', '..', '..', `${DllFolder}`).replace(/\\/g, '\\\\')
   process.env.__updateFolder = join(__dirname, '..', '..', '..', '..', `${HotUpdateFolder}`).replace(/\\/g, '\\\\')
 } else {
-  __updateFolder = join(__dirname, '..', '..', '..', `${HotUpdateFolder}`).replace(/\\/g, '\\\\')
+  process.env.__updateFolder = join(__dirname, '..', '..', '..', `${HotUpdateFolder}`).replace(/\\/g, '\\\\')
 }
 export const winURL = process.env.NODE_ENV === 'development' ? `http://localhost:${process.env.PORT}` : `file://${join(__dirname, '..', 'renderer', 'index.html')}`
-export const loadingURL = process.env.NODE_ENV === 'development' ? `http://localhost:${process.env.PORT}/loader.html` : `file://${__static}/loader.html`
-export const lib = __lib
-export const updateFolder = __updateFolder
+export const loadingURL = process.env.NODE_ENV === 'development' ? `http://localhost:${process.env.PORT}/loader.html` : `file://${process.env.__static}/loader.html`
+export const lib = process.env.__lib
+export const updateFolder = process.env.__updateFolder
