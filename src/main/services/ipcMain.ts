940a6bd4e01d4124dec2aebbed99643bb6059b81
umbrella22 <syl1998@hotmail.com>
2021-10-31 15:42:14 +0800

    fix(main):Decoupling download function

--- src/main/services/ipcMain.ts
+++ src/main/services/ipcMain.ts
@@ -1,30 +1,34 @@
-import { ipcMain, dialog, BrowserWindow } from 'electron'
+import { ipcMain, dialog, BrowserWindow, app } from 'electron'
 import Server from '../server'
 import { winURL } from '../config/StaticPath'
 import { updater } from './HotUpdater'
+import DownloadFile from './downloadFile'
 
 export default {
-  Mainfunc(mainWindow: BrowserWindow, IsUseSysTitle: Boolean) {
+  Mainfunc(IsUseSysTitle: Boolean) {
     ipcMain.handle('IsUseSysTitle', async () => {
       return IsUseSysTitle
     })
-    ipcMain.handle('windows-mini', () => {
-      mainWindow.minimize()
+    ipcMain.handle('windows-mini', (event, args) => {
+      BrowserWindow.fromWebContents(event.sender)?.minimize()
     })
-    ipcMain.handle('window-max', async () => {
-      if (mainWindow.isMaximized()) {
-        mainWindow.restore()
+    ipcMain.handle('window-max', async (event, args) => {
+      if (BrowserWindow.fromWebContents(event.sender)?.isMaximized()) {
+        BrowserWindow.fromWebContents(event.sender)?.restore()
         return { status: false }
       } else {
-        mainWindow.maximize()
+        BrowserWindow.fromWebContents(event.sender)?.maximize()
         return { status: true }
       }
     })
-    ipcMain.handle('window-close', () => {
-      mainWindow.close()
+    ipcMain.handle('window-close', (event, args) => {
+      BrowserWindow.fromWebContents(event.sender)?.close()
+    })
+    ipcMain.handle('app-close', (event, args) => {
+      app.quit()
     })
     ipcMain.handle('open-messagebox', async (event, arg) => {
-      const res = await dialog.showMessageBox(mainWindow, {
+      const res = await dialog.showMessageBox(BrowserWindow.fromWebContents(event.sender), {
         type: arg.type || 'info',
         title: arg.title || '',
         buttons: arg.buttons || [],
@@ -63,7 +67,10 @@ export default {
       }
     })
     ipcMain.handle('hot-update', (event, arg) => {
-      updater(mainWindow)
+      updater(BrowserWindow.fromWebContents(event.sender))
+    })
+    ipcMain.handle('start-download', (event, msg) => {
+      new DownloadFile(BrowserWindow.fromWebContents(event.sender), msg.downloadUrl).start()
     })
     ipcMain.handle('open-win', (event, arg) => {
       const ChildWin = new BrowserWindow({
@@ -74,9 +81,9 @@ export default {
         minWidth: 842,
         show: false,
         webPreferences: {
-          // nodeIntegration: true,
-          // contextIsolation: false,
-          // webSecurity: false,
+          nodeIntegration: true,
+          contextIsolation: false,
+          webSecurity: false,
           // 如果是开发模式可以使用devTools
           devTools: process.env.NODE_ENV === 'development',
           // devTools: true,
