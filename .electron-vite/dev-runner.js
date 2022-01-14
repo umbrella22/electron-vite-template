95ecf0e84135de7ed19e32ddcb904fc40b006c27
umbrella22 <1035765115@qq.com>
2022-01-14 11:31:22 +0800

    fix(main): chinese log lose

--- .electron-vite/dev-runner.js
+++ .electron-vite/dev-runner.js
@@ -176,15 +176,13 @@ function electronLog(data, color) {
         data.forEach(line => {
             log += `  ${line}\n`
         })
-        if (/[0-9A-z]+/.test(log)) {
-            console.log(
-                chalk[color].bold(`┏ ${config.dev.chineseLog ? '主程序日志' : 'Electron'} -------------------`) +
-                '\n\n' +
-                log +
-                chalk[color].bold('┗ ----------------------------') +
-                '\n'
-            )
-        }
+        console.log(
+            chalk[color].bold(`┏ ${config.dev.chineseLog ? '主程序日志' : 'Electron'} -------------------`) +
+            '\n\n' +
+            log +
+            chalk[color].bold('┗ ----------------------------') +
+            '\n'
+        )
     }
 
 }
