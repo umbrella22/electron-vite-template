cf6a33e9f00100349cb46fee0fa1f30e1e02bf54
umbrella22 <1035765115@qq.com>
2022-01-14 11:35:43 +0800

    fix(main): path change

--- src/main/config/StaticPath.ts
+++ src/main/config/StaticPath.ts
@@ -2,16 +2,28 @@
 import { join } from 'path'
 import { DllFolder, HotUpdateFolder } from '@config/index'
 
+const filePath = {
+  winURL: {
+    development: `http://localhost:${process.env.PORT}`,
+    production: `file://${join(__dirname, '..', 'renderer', 'index.html')}`
+  },
+  loadingURL: {
+    development: `http://localhost:${process.env.PORT}/loader.html`,
+    production: `file://${join(__dirname, '..', 'renderer', 'loader.html')}`
+  },
+}
+
 if (process.env.NODE_ENV !== 'development') process.env.__static = join(__dirname, '..', 'renderer').replace(/\\/g, '\\\\');
 
 process.env.__lib = getAppRootPath(DllFolder)
 process.env.__updateFolder = getAppRootPath(HotUpdateFolder)
 
-export const winURL = process.env.NODE_ENV === 'development' ? `http://localhost:${process.env.PORT}` : `file://${join(__dirname, '..', 'renderer', 'index.html')}`
-export const loadingURL = process.env.NODE_ENV === 'development' ? `http://localhost:${process.env.PORT}/loader.html` : `file://${process.env.__static}/loader.html`
+export const winURL = filePath.winURL[process.env.NODE_ENV]
+export const loadingURL = filePath.loadingURL[process.env.NODE_ENV]
 export const lib = process.env.__lib
 export const updateFolder = process.env.__updateFolder
 
 function getAppRootPath(path: string) {
   return process.env.NODE_ENV !== 'development' ? join(__dirname, '..', '..', '..', '..', path).replace(/\\/g, '\\\\') : join(__dirname, '..', '..', '..', path).replace(/\\/g, '\\\\')
 }
+
