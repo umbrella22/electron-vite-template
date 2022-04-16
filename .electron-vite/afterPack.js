574a469d7eeff9aec8f7d7f499675b8d861ed2f7
Sky <syl1998@hotmail.com>
2022-04-16 16:24:25 +0800

    fix(config):  Fix packaging failure

--- .electron-vite/afterPack.js
+++ .electron-vite/afterPack.js
@@ -1,6 +1,6 @@
 // pack后对不同位数的系统迁移不同的文件
 'use strict';
-const { copySync } = require('fs-extra');
+const { copySync, ensureDirSync } = require('fs-extra');
 const { join } = require('path');
 const { Arch } = require('electron-builder');
 // type ElectronPlatformName = "darwin" | "linux" | "win32" | "mas"
@@ -8,6 +8,9 @@ exports.default = async context => {
   const LIB_OUTPUT_DIR = context.appOutDir;
   const LIB_INPUT_DIR = join("rootLib", context.electronPlatformName, Arch[context.arch]);
   const LIB_COMMON_INPUT_DIR = join("rootLib", "common");
+  // 确保文件夹存在
+  ensureDirSync(LIB_COMMON_INPUT_DIR);
+  ensureDirSync(LIB_INPUT_DIR);
   // 移动文件文件
   copySync(LIB_INPUT_DIR, LIB_OUTPUT_DIR);
   copySync(LIB_COMMON_INPUT_DIR, LIB_OUTPUT_DIR);
