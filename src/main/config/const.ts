93e7f4d688b909ba4ed367c43621cf4035d2835d
umbrella22 <1035765115@qq.com>
2023-03-02 15:45:50 +0800

    feat: use dotenv

---
+++ src/main/config/const.ts
@@ -0,0 +1,8 @@
+export const UseStartupChart: boolean = true;
+export const IsUseSysTitle: boolean = false;
+export const BuiltInServerPort: number = 25565;
+export const hotPublishUrl: string =
+  "http://umbrella22.github.io/electron-vite-template";
+export const hotPublishConfigName: string = "update-config";
+export const openDevTools: boolean = false;
+export const DisableF12: boolean = true;
--- src/main/services/printHandle.ts
+++ src/main/services/printHandle.ts
@@ -1,41 +1,47 @@
-import { BrowserWindow, ipcMain, WebContentsPrintOptions } from 'electron'
-import config from '@config/index'
-import { otherWindowConfig } from "../config/windowsConfig"
-import { printURL } from '@main/config/StaticPath'
+import { BrowserWindow, ipcMain, WebContentsPrintOptions } from "electron";
+import { IsUseSysTitle } from "../config/const";
+import { otherWindowConfig } from "../config/windowsConfig";
+import { printURL } from "@main/config/StaticPath";
 
 export function usePrintHandle() {
-  ipcMain.handle('getPrinters', async event => {
-    return await event.sender.getPrintersAsync()
-  })
+  ipcMain.handle("getPrinters", async (event) => {
+    return await event.sender.getPrintersAsync();
+  });
 
-  ipcMain.handle('printHandlePrint', async (event, options: WebContentsPrintOptions) => {
-    return new Promise(resolve => {
-      event.sender.print(options, (success: boolean, failureReason: string) => {
-        resolve({ success, failureReason })
-      })
-    })
-  })
+  ipcMain.handle(
+    "printHandlePrint",
+    async (event, options: WebContentsPrintOptions) => {
+      return new Promise((resolve) => {
+        event.sender.print(
+          options,
+          (success: boolean, failureReason: string) => {
+            resolve({ success, failureReason });
+          }
+        );
+      });
+    }
+  );
 
-  ipcMain.handle('openPrintDemoWindow', () => {
-    openPrintDemoWindow()
-  })
+  ipcMain.handle("openPrintDemoWindow", () => {
+    openPrintDemoWindow();
+  });
 }
 
-let win: BrowserWindow
+let win: BrowserWindow;
 export function openPrintDemoWindow() {
   if (win) {
-    win.show()
-    return
+    win.show();
+    return;
   }
   win = new BrowserWindow({
-    titleBarStyle: config.IsUseSysTitle ? 'default' : 'hidden',
-    ...Object.assign(otherWindowConfig, {})
-  })
-  win.loadURL(printURL)
-  win.on('ready-to-show', () => {
-    win.show()
-  })
-  win.on('closed', () => {
-    win = null
-  })
+    titleBarStyle: IsUseSysTitle ? "default" : "hidden",
+    ...Object.assign(otherWindowConfig, {}),
+  });
+  win.loadURL(printURL);
+  win.on("ready-to-show", () => {
+    win.show();
+  });
+  win.on("closed", () => {
+    win = null;
+  });
 }
