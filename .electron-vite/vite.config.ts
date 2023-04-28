2d33a47d0c50d13831c747816f445f8b91cc7307
umbrella22 <1035765115@qq.com>
2023-04-28 17:18:15 +0800

    chore: dep upgrade

--- .electron-vite/vite.config.ts
+++ .electron-vite/vite.config.ts
@@ -1,46 +1,43 @@
-import { join } from 'path'
-import { defineConfig } from 'vite'
-import vuePlugin from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import userConfig from '../config'
+import { join } from "path";
+import { defineConfig } from "vite";
+import vuePlugin from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import userConfig from "../config";
 
-const IsWeb = process.env.BUILD_TARGET === 'web'
+const IsWeb = process.env.BUILD_TARGET === "web";
 
 function resolve(dir: string) {
-    return join(__dirname, '..', dir)
+  return join(__dirname, "..", dir);
 }
-userConfig.build.env.is_web = IsWeb
-userConfig.dev.env.is_web = IsWeb
+userConfig.build.env.is_web = IsWeb;
+userConfig.dev.env.is_web = IsWeb;
 
-const root = resolve('src/renderer')
+const root = resolve("src/renderer");
 
 export default defineConfig({
-    mode: process.env.NODE_ENV,
-    root,
-    define: {
-        'process.env': process.env.NODE_ENV === 'production' ? userConfig.build.env : userConfig.dev.env,
+  mode: process.env.NODE_ENV,
+  root,
+  define: {
+    "process.env":
+      process.env.NODE_ENV === "production"
+        ? userConfig.build.env
+        : userConfig.dev.env,
+  },
+  resolve: {
+    alias: {
+      "@renderer": root,
+      "@store": join(root, "/store/modules"),
     },
-    resolve: {
-        alias: {
-            '@renderer': root,
-            '@store': join(root, '/store/modules'),
-        }
-    },
-    base: './',
-    build: {
-        outDir: IsWeb ? resolve('dist/web') : resolve('dist/electron/renderer'),
-        emptyOutDir: true,
-        target: 'esnext',
-        minify: 'esbuild',
-        cssCodeSplit:false
-    },
-    server: {
-    },
-    plugins: [
-        vueJsx(),
-        vuePlugin()
-    ],
-    optimizeDeps: {
-    },
-    publicDir: resolve('static')
-})
+  },
+  base: "./",
+  build: {
+    outDir: IsWeb ? resolve("dist/web") : resolve("dist/electron/renderer"),
+    emptyOutDir: true,
+    target: "esnext",
+    minify: "esbuild",
+    cssCodeSplit: false,
+  },
+  server: {},
+  plugins: [vueJsx(), vuePlugin()],
+  optimizeDeps: {},
+});
