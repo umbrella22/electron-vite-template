8ef1e75aac53ab36e851224010e0992983257313
umbrella22 <1035765115@qq.com>
2023-11-19 17:38:45 +0800

    fix: see CHANGELOG

--- .electron-vite/build.ts
+++ .electron-vite/build.ts
@@ -2,145 +2,39 @@ process.env.NODE_ENV = "production";
 
 import { join } from "path";
 import { say } from "cfonts";
-import inquirer from "inquirer";
-import { deleteSync } from "del";
-import { build as viteBuild } from "vite";
-import { Platform, build, Arch } from "electron-builder";
-import type { Configuration } from "electron-builder";
-import buildConfig from "../build.json";
+import { deleteAsync } from "del";
+import { build } from "vite";
 import chalk from "chalk";
 import { rollup, OutputOptions } from "rollup";
 import { Listr } from "listr2";
 import rollupOptions from "./rollup.config";
 import { errorLog, doneLog } from "./log";
 
-const [, , arch] = process.argv;
-const EOL = process.platform === "win32" ? "\r\n" : "\n";
-
 const mainOpt = rollupOptions(process.env.NODE_ENV, "main");
 const preloadOpt = rollupOptions(process.env.NODE_ENV, "preload");
 const isCI = process.env.CI || false;
 
-const [optional, linuxOptional, winOptional] = [
-  ["web", "win", "mac", "linux"],
-  ["AppImage", "snap", "deb", "rpm", "pacman"],
-  ["win-any", "win32", "win64", "winp-any", "winp32", "winp64"],
-];
+if (process.env.BUILD_TARGET === "web") web();
+else unionBuild();
 
-function platformOptional() {
-  switch (process.platform) {
-    case "win32":
-      return ["web", ...optional.filter((item) => item.startsWith("win"))];
-    case "linux":
-      return ["web", ...optional.filter((item) => !(item === "mac"))];
-    default:
-      return [...optional];
-  }
-}
-
-function defaultPlatform() {
-  switch (process.platform) {
-    case "win32":
-      return "win";
-    case "darwin":
-      return "mac";
-    default:
-      return process.platform;
-  }
-}
-
-const question = async (): Promise<string> =>
-  inquirer
-    .prompt({
-      type: "list",
-      name: "platform",
-      message: "Which platform is you want to build?",
-      choices: platformOptional(),
-      default: defaultPlatform(),
-    })
-    .then(({ platform }) => platform);
-
-const linuxQuestion = async (): Promise<string> =>
-  inquirer
-    .prompt({
-      type: "list",
-      name: "platform",
-      message: "Please select linux package",
-      choices: linuxOptional,
-      default: "AppImage",
-    })
-    .then(({ platform }) => platform);
-
-const winQuestion = async (): Promise<string> =>
-  inquirer
-    .prompt({
-      type: "list",
-      name: "platform",
-      message: "Please select win package",
-      choices: winOptional,
-      default: "win-any",
-    })
-    .then(({ platform }) => platform);
-
-process.env.BUILD_TARGET === "onlyClean" && clean();
-
-preBuild();
-
-function clean() {
-  deleteSync([
-    "dist/electron/main/**",
-    "dist/electron/renderer/**",
-    "dist/web/**",
-    "build/**",
+async function clean() {
+  await deleteAsync([
+    "dist/electron/main/*",
+    "dist/electron/renderer/*",
+    "dist/web/*",
+    "build/*",
     "!build/icons",
     "!build/lib",
     "!build/lib/electron-build.*",
     "!build/icons/icon.*",
-    "!dist/electron/main",
-    "!dist/electron/renderer",
-    "!dist/web",
   ]);
-  doneLog("clear done");
+  doneLog(`\nclear done`);
   if (process.env.BUILD_TARGET === "onlyClean") process.exit();
 }
 
-async function preBuild() {
-  let _arch = "";
-  if (arch)
-    !platformOptional().includes(arch.trim().toLowerCase())
-      ? (_arch = await question())
-      : (_arch = arch);
-  else _arch = await question();
-  let archTag: null | Map<Platform, Map<Arch, Array<string>>> = null;
-  switch (_arch) {
-    case "web":
-      return web();
-    case "win":
-      archTag = Platform.WINDOWS.createTarget();
-      _arch = await winQuestion();
-      let bv = {
-        target: "nsis",
-        arch: [] as string[],
-      };
-      _arch.startsWith("winp") && (bv.target = "portable");
-      if (_arch.includes("any")) bv.arch = ["x64", "ia32"];
-      else if (_arch.includes("32")) bv.arch = ["ia32"];
-      else if (_arch.includes("64")) bv.arch = ["x64"];
-      buildConfig.win["target"] = [bv];
-      break;
-    case "mac":
-      archTag = Platform.MAC.createTarget();
-      break;
-    case "linux":
-      archTag = Platform.LINUX.createTarget();
-      buildConfig.linux["target"] = await linuxQuestion();
-  }
-  archTag && unionBuild(archTag);
-}
-
-function unionBuild(archTag: Map<Platform, Map<Arch, Array<string>>>) {
+async function unionBuild() {
   greeting();
-  if (process.env.BUILD_TARGET === "clean") clean();
+  await clean();
 
   const tasksLister = new Listr(
     [
@@ -152,8 +46,7 @@ function unionBuild(archTag: Map<Platform, Map<Arch, Array<string>>>) {
             await build.write(mainOpt.output as OutputOptions);
           } catch (error) {
             console.error(`\n${error}\n`);
-            errorLog("failed to build main process");
-            process.exit(1);
+            errorLog(`\n  failed to build main process`);
           }
         },
       },
@@ -168,52 +61,36 @@ function unionBuild(archTag: Map<Platform, Map<Arch, Array<string>>>) {
             errorLog("failed to build main process");
             process.exit(1);
           }
-        },
+        }
       },
       {
         title: "building renderer process",
-        task: async () => {
-          try {
-            await viteBuild({ configFile: join(__dirname, "vite.config.ts") });
-          } catch (error) {
-            console.error(`\n${error}\n`);
-            errorLog("failed to build renderer process");
-            process.exit(1);
-          }
-        },
-        options: { persistentOutput: true },
-      },
-      {
-        title: "building package",
         task: async (_, tasks) => {
           try {
-            await build({
-              targets: archTag,
-              config: buildConfig as Configuration,
-            });
-            tasks.output = `${
-              chalk.bgBlue.white(" OKAY ") + " "
-            }All done${EOL}`;
+            await build({ configFile: join(__dirname, "vite.config.ts") });
+            tasks.output = `take it away ${chalk.yellow(
+              "`electron-builder`"
+            )}\n`;
           } catch (error) {
             console.error(`\n${error}\n`);
-            errorLog("failed to build electron process");
-            process.exit(1);
+            errorLog(`\n  failed to build renderer process`);
           }
         },
         options: { persistentOutput: true },
       },
     ],
     {
-      exitOnError: false,
+      exitOnError: true,
     }
   );
-  tasksLister.run();
+  await tasksLister.run();
+  
 }
 
-function web() {
-  deleteSync(["dist/web/*", "!.gitkeep"]);
-  viteBuild({ configFile: join(__dirname, "vite.config.ts") }).then((res) => {
-    doneLog("RendererProcess build success");
+async function web() {
+  await deleteAsync(["dist/web/*", "!.gitkeep"]);
+  build({ configFile: join(__dirname, "vite.config.ts") }).then((res) => {
+    doneLog(`web build success`);
     process.exit();
   });
 }
--- src/main/config/StaticPath.ts
+++ src/main/config/StaticPath.ts
@@ -3,6 +3,8 @@ import { join } from 'path'
 import config from '@config/index'
 import { app } from 'electron'
 
+const env = app.isPackaged ? 'production': 'development'
+
 const filePath = {
   winURL: {
     development: `http://localhost:${process.env.PORT}`,
@@ -13,7 +15,7 @@ const filePath = {
     production: `file://${join(app.getAppPath(), "dist", "electron", 'renderer', 'loader.html')}`
   },
   getPreloadFile(fileName: string) {
-    if (process.env.NODE_ENV !== 'development') {
+    if (env !== 'development') {
       return join(app.getAppPath(), "dist", "electron", "main", `${fileName}.js`)
     }
     return join(app.getAppPath(), `${fileName}.js`)
@@ -21,18 +23,18 @@ const filePath = {
   }
 }
 
-if (process.env.NODE_ENV !== 'development') process.env.__static = join(app.getAppPath(), "dist", "electron", 'renderer').replace(/\\/g, '\\\\');
+if (env !== 'development') process.env.__static = join(app.getAppPath(), "dist", "electron", 'renderer').replace(/\\/g, '\\\\');
 
 
 process.env.__lib = getAppRootPath(config.DllFolder)
 process.env.__updateFolder = getAppRootPath(config.HotUpdateFolder)
 
 function getAppRootPath(path: string) {
-  return process.env.NODE_ENV !== 'development' ? join(__dirname, '..', '..', '..', '..', path).replace(/\\/g, '\\\\') : join(__dirname, '..', '..', '..', path).replace(/\\/g, '\\\\')
+  return env !== 'development' ? join(__dirname, '..', '..', '..', '..', path).replace(/\\/g, '\\\\') : join(__dirname, '..', '..', '..', path).replace(/\\/g, '\\\\')
 }
 
-export const winURL = filePath.winURL[process.env.NODE_ENV]
-export const loadingURL = filePath.loadingURL[process.env.NODE_ENV]
+export const winURL = filePath.winURL[env]
+export const loadingURL = filePath.loadingURL[env]
 export const lib = process.env.__lib
 export const updateFolder = process.env.__updateFolder
 export const getPreloadFile = filePath.getPreloadFile
