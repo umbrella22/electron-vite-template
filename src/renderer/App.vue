73f4cbbeef490b82faf3b8aa1e58e1e0f2dad5b9
umbrella22 <1035765115@qq.com>
2023-11-20 13:58:38 +0800

    fix:fix code

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,15 +1,13 @@
 <template>
   <title-bar />
-    <router-view v-slot="{ Component }">
-      <component :is="Component" />
-    </router-view>
+  <router-view v-slot="{ Component }">
+    <component :is="Component" />
+  </router-view>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
 import TitleBar from "./components/common/TitleBar.vue";
 
-const i18nt = computed(() => i18n.global.messages[i18n.global.locale].el);
 </script>
 
 <style></style>
