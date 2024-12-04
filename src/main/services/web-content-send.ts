78d4ec62d13d4f992d938b3eb59ac691ffc4a081
orzi <1063614727@qq.com>
2024-12-04 16:11:36 +0800

    1

--- src/main/services/web-content-send.ts
+++ src/main/services/web-content-send.ts
@@ -1,5 +1,10 @@
 import { IWebContentSend } from "src/ipc";
 
+/**
+ * 主进程给渲染进程发消息
+ * 这是一个虚拟化的对象，实际上并没有实现任何方法，也不需要任何实现
+ * 主进程给渲染进程发消息的话，直接就 webContentSend.事件名 就行了
+ */
 export const webContentSend: IWebContentSend = new Proxy(
   {},
   {
